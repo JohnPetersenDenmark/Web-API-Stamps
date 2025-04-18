5188b0546a21ecd96dd0ebdd21b9295e01f59149
ringstedstamps <stampringstedogomegn@gmail.com>
2025-04-18 20:09:01 +0200

    ok

--- Web API Stamps/Models/StampCategoryDto.cs
+++ Web API Stamps/Models/StampCategoryDto.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CustomerWaypointApp.Models
 {
     public class StampCategoryDto
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
 
+        [JsonPropertyName("category")]
         public string Category { get; set; }        
     }
 }
